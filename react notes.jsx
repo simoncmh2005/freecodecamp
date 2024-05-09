6da36be5e846ec6292079f3a8f65f24f75c475c9
simoncmh2005 <112465978+simoncmh2005@users.noreply.github.com>
2024-05-09 11:23:41 +0800

    Add files via upload

---
+++ react notes.jsx
@@ -0,0 +1,7 @@
+const jsxJSX = <h1>Hello JSX!</h1>
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
+
+class TypesOfFood extends React.Component {
+    constructor(props) {
+      super(props);
+    }
